79fc316787f921e4ad4885633be30672eff52f84
Eric Daniels <edaniels@bluemoonforms.com>
2025-05-02 12:28:54 -0500

    shadows looking pretty good, want to add multiple maps per light source to get better coverage

--- twgl/renderer.js
+++ twgl/renderer.js
@@ -3,8 +3,13 @@ import { DefaultControls } from './default-controls.js';
 import * as twgl from './twgl_lib/twgl-full.module.js';
 
 export class Renderer {
+  /**
+   * Initialize parameters to render the given scene
+   */
   constructor(/**@type{Scene}*/scene) {
     const canvas = document.querySelector('canvas');
+    twgl.resizeCanvasToDisplaySize(canvas);
+    const resolution = Math.max(canvas.width, canvas.height);
     /**@type{WebGL2RenderingContext}*/this.ctx = canvas.getContext('webgl2');
     /**@type{Object.<string, ShaderInfo>}*/this.shaders = {};
     /**@type{ShaderInfo}*/this.currentShader = null;
@@ -16,61 +21,88 @@ export class Renderer {
         dir: scene.options?.lighting?.d ? twgl.v3.normalize(scene.options.lighting.d) : [0, -1, 0],
         mag: scene.options?.lighting?.di ?? (scene.options?.lighting?.d ? twgl.v3.length(scene.options.lighting.d) : 1),
       },
-      s: scene.options?.lighting?.s,
+      diffuseAsAmbient: scene.options?.lighting?.diffuseAsAmbient,
+      s: scene.options?.lighting?.s || [],
+      outsideShadowMapCoef: scene.options?.lighting?.outsideShadowMapCoef ?? 1.0,
+      shadowCoef: scene.options?.lighting?.shadowCoef ?? 0.85,
+      shadowBias: (scene.options?.lighting?.shadowBias ?? 1.2) / resolution,
     };
     this.options = scene.options;
-    twgl.resizeCanvasToDisplaySize(canvas);
     this.ctx.viewport(0, 0, canvas.width, canvas.height);
-    this.makePerspectiveMatrix();
+    this.matP = this.#makePerspectiveMatrix();
     this.viewpoint = twgl.m4.lookAt([0, 0, 0], [0, 0, -1], [0, 1, 0]);
     this.matWorld = twgl.m4.inverse(this.viewpoint);
     this.matRotation = twgl.m4.setTranslation(this.matWorld, [0, 0, 0]);
     this.controls = new DefaultControls(this.viewpoint, scene.options?.unlockHeight, scene.options?.unlockUp);
     this.ctx.enable(this.ctx.DEPTH_TEST);
-    // this.ctx.clearColor( // doesn't work, presumably twgl is either overwriting it or not clearing the color buffer?
-    //   this.options.bgColor[0] || 1.0,
-    //   this.options.bgColor[1] || 1.0,
-    //   this.options.bgColor[2] || 1.0,
-    //   this.options.bgColor[3] || 1.0,
-    // );
+    this.#initShadowMaps();
+    this.ctx.clearColor(
+      this.options?.bgColor?.[0] || 1.0,
+      this.options?.bgColor?.[1] || 1.0,
+      this.options?.bgColor?.[2] || 1.0,
+      this.options?.bgColor?.[3] || 1.0,
+    );
 
     // temp; put some ui controls in at some point
     window.lighting = this.lighting
   }
 
+  /**
+   * Render the scene
+   */
   async start() {
     await Promise.all([
-      this.loadModels(),
-      this.useProgram('default'),
+      this.#loadModels(),
+      this.#resolveProgram('default'),
+      this.#resolveProgram('zmap'),
     ]);
-    requestAnimationFrame(t => this.render(t));
+    this.#drawShadowMaps();
+    requestAnimationFrame(t => this.#render(t));
   }
 
-  render(time) {
+  #render(time) {
     if (twgl.resizeCanvasToDisplaySize(this.ctx.canvas)) {
-      this.makePerspectiveMatrix();
+      this.#makePerspectiveMatrix();
       this.ctx.viewport(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     }
-    const changed = this.updateMatrices(time);
+    const changed = this.#updateMatrices(time);
     if (!changed) {
-      return requestAnimationFrame(t => this.render(t));
+      return requestAnimationFrame(t => this.#render(t));
     }
+    this.ctx.clear(this.ctx.COLOR_BUFFER_BIT | this.ctx.DEPTH_BUFFER_BIT);
 
-    // probably move this to its own function that can accept & set a shader program before drawing
-    twgl.setUniforms(this.currentShader, {
+    // // probably move this to its own function that can accept & set a shader program before drawing
+    this.#useProgram('default');
+    const uniforms = {
       uMatP: this.matP,
       uMatR: this.matRotation,
       uLa: this.lighting.a,
       uLdd: twgl.m4.transformDirection(this.matRotation, this.lighting.d.dir),
       uLdm: this.lighting.d.mag,
-      uLp0: twgl.m4.transformPoint(this.matWorld, this.lighting.s[0].slice(0, 3)),
-      uLp0b: this.lighting.s[0][3],
-      uLp0color: this.getPointLightColor(0),
-      uLp1: twgl.m4.transformPoint(this.matWorld, this.lighting.s[1].slice(0, 3)),
-      uLp1b: this.lighting.s[1][3],
-      uLp1color: this.getPointLightColor(1),
-      uPointLightDecay: 1.5708, // pi / 2
-    });
+      uOutsideShadowMapCoef: this.lighting.outsideShadowMapCoef,
+      uShadowCoef: this.lighting.shadowCoef,
+      uShadowBias: this.lighting.shadowBias,
+    };
+    if (this.lighting.s[0]) {
+      Object.assign(uniforms, {
+        uLp0: twgl.m4.transformPoint(this.matWorld, this.lighting.s[0].position),
+        uLp0b: this.lighting.s[0].brightness,
+        uLp0color: this.lighting.s[0].color ?? [1, 1, 1],
+        uMatST0: this.lighting.s[0].matST,
+        uPointLightDecay: Math.PI * 4,// probably make it configurable
+        uShadowMap0: this.lighting.s[0].shadowBufferInfo.attachments[0],
+      });
+    }
+    if (this.lighting.s[1]) {
+      Object.assign(uniforms, {
+        uLp1: twgl.m4.transformPoint(this.matWorld, this.lighting.s[1].position),
+        uLp1b: this.lighting.s[1].brightness,
+        uLp1color: this.lighting.s[1].color ?? [1, 1, 1],
+        uMatST1: this.lighting.s[1].matST,
+        uShadowMap1: this.lighting.s[1].shadowBufferInfo.attachments[0],
+      });
+    }
+    twgl.setUniformsAndBindTextures(this.currentShader, uniforms);
     for (const model of this.models) {
       twgl.setUniforms(this.currentShader, {
         uMatMV: twgl.m4.multiply(this.matWorld, model.matrix),
@@ -78,23 +110,22 @@ export class Renderer {
       for (const group of model.groups) {
         const noMtlColor = group.color || model.color || [0.8, 0.8, 0.8];
         twgl.setBuffersAndAttributes(this.ctx, this.currentShader, group.bufferInfo);
-        twgl.setUniforms(this.currentShader, {
-          uKa: group.material?.Ka || noMtlColor,
+        twgl.setUniformsAndBindTextures(this.currentShader, {
+          uKa: this.#getAmbientColor(group.material) || noMtlColor,
           uKd: twgl.v3.divScalar((group.material?.Kd || noMtlColor), Math.PI),
           uKs: twgl.v3.divScalar((group.material?.Ks || [0, 0, 0]), Math.PI),
           uNs: group.material?.Ns || 0,
+          uOpacity: group.material?.d || 1.0,
+          uTexture: group.texture || this.whiteTexture,
         });
-        // there's probably a twgl function for textures
-        this.ctx.activeTexture(this.ctx.TEXTURE0);
-        this.ctx.bindTexture(this.ctx.TEXTURE_2D, group.texture || this.whiteTexture);
         twgl.drawBufferInfo(this.ctx, group.bufferInfo);
       }
     }
 
-    requestAnimationFrame(t => this.render(t));
+    requestAnimationFrame(t => this.#render(t));
   }
 
-  updateMatrices(t) {
+  #updateMatrices(t) {
     // t is ms since last frame (roughly 8 as long as nothing crazy is happening)
     let changed = this.controls.updateTime(t);
     if (changed) {
@@ -102,32 +133,48 @@ export class Renderer {
       twgl.m4.inverse(this.viewpoint, this.matWorld);
       twgl.m4.copy(this.matWorld, this.matRotation);
       twgl.m4.setTranslation(this.matRotation, [0, 0, 0], this.matRotation)
+
+      for (const light of this.lighting.s) {
+        twgl.m4.translate(twgl.m4.identity(), [0.5, 0.5, 0.5], light.matST);
+        twgl.m4.scale(light.matST, [0.5, 0.5, 0.5], light.matST);
+        twgl.m4.multiply(light.matST, light.matP, light.matST);
+        twgl.m4.multiply(light.matST, light.matWorld, light.matST);
+        twgl.m4.multiply(light.matST, twgl.m4.inverse(this.matWorld), light.matST);
+      }
     }
     changed ||= this.drawNext;
     this.drawNext = false;
     return changed;
   }
 
-  async useProgram(/**@type{string}*/name) {
+  #useProgram(/**@type{string}*/name) {
     if (this.currentShader?.name == name) {
-      return Promise.resolve();
+      return true;
     }
-    await this.resolveProgram(name);
+    if (!this.shaders[name]) {
+      throw new Error(`Shader [${name}] is not loaded.`);
+    }
     this.currentShader = this.shaders[name];
     this.ctx.useProgram(this.currentShader.program);
   }
 
-  async resolveProgram(/**@type{string}*/name) {
+  async #resolveProgram(/**@type{string}*/name) {
     if (this.shaders[name]) {
       return Promise.resolve();
     }
     const vs = await (await fetch(`./shaders/${name}/vs.gl`)).text();
     const fs = await (await fetch(`./shaders/${name}/fs.gl`)).text();
-    this.shaders[name] = await twgl.createProgramInfoAsync(this.ctx, [vs, fs]);
+    this.shaders[name] = await twgl.createProgramInfoAsync(this.ctx, [vs, fs], {
+      attribLocations: {
+        aVertex: 0,
+        aNormal: 1,
+        aTexCoord: 2,
+      },
+    });
     this.shaders[name].name = name;
   }
 
-  async loadModels() {
+  async #loadModels() {
     if (!this.modelSources) {
       throw new Error('Nothing to render');
     }
@@ -143,8 +190,8 @@ export class Renderer {
         // maybe handle cases with both map_Ka and map_Kd
         const src = group.material?.map_Kd || group.material?.map_Ka;
         if (src) {
-          group.texture = twgl.createTexture(this.ctx, { src },
-            () => this.drawNext = true);//this.loadMtlTexture(group.material);
+          group.texture = twgl.createTexture(this.ctx,
+            { src, flipY: true }, () => this.drawNext = true);
         }
       }
     }
@@ -153,18 +200,68 @@ export class Renderer {
     this.whiteTexture = twgl.createTexture(this.ctx, { src });
   }
 
-  makePerspectiveMatrix() {
+  #makePerspectiveMatrix() {
     const aspect = this.ctx.canvas.width / this.ctx.canvas.height;
     const fovW = this.options?.fov || Math.PI / 3;
     const fovH = fovW / aspect;
     const near = this.options?.nearClip || 0.1;
     const far = this.options?.farClip || 100;
-    this.matP = twgl.m4.perspective(fovH, aspect, near, far, this.matP);
+    return twgl.m4.perspective(fovH, aspect, near, far, this.matP);
   }
 
-  getPointLightColor(i) {
-    const light = this.lighting.s[i];
-    return [light[4] || 1, light[5] || 1, light[6] || 1];
+  #initShadowMaps() {
+    const attachments = [{
+      attachmentPoint: this.ctx.DEPTH_ATTACHMENT,
+      format: this.ctx.DEPTH_COMPONENT,
+      internalFormat: this.ctx.DEPTH_COMPONENT32F,
+      minMag: this.ctx.NEAREST,
+    }];
+    for (const light of this.lighting.s) {
+      const resolution = light.resolution ?? 2048; // support different x/y dimensions?
+      const fov = light.fov ?? Math.PI / 1.2;
+      const clipNear = light.clipNear ?? 0.1;
+      const clipFar = light.clipFar ?? 25;
+      const lookAt = light.lookAt ?? (p => {p[1] *= -1; return p;})(light.position.slice());
+      const upVec = light.upVec ?? [0, 0, 1];
+      light.shadowBufferInfo = twgl.createFramebufferInfo(this.ctx, attachments, resolution, resolution);
+      light.matP = twgl.m4.perspective(fov, 1, clipNear, clipFar);
+      light.matMV = twgl.m4.create();
+      light.matWorld = twgl.m4.inverse(twgl.m4.lookAt(light.position, lookAt, upVec));
+      light.matST = twgl.m4.identity();
+    }
+  }
+
+  #drawShadowMaps() {
+    this.#useProgram('zmap');
+    for (const light of this.lighting.s) {
+      twgl.bindFramebufferInfo(this.ctx, light.shadowBufferInfo);
+      twgl.setUniforms(this.currentShader, {
+        uMatP: light.matP,
+      });
+      for (const model of this.models) {
+        twgl.setUniforms(this.currentShader, {
+          uMatMV: twgl.m4.multiply(light.matWorld, model.matrix, light.matMV),
+        });
+        for (const group of model.groups) {
+          twgl.setBuffersAndAttributes(this.ctx, this.currentShader, group.bufferInfo);
+          twgl.drawBufferInfo(this.ctx, group.bufferInfo);
+        }
+      }
+    }
+    // TODO one for the directional light
+    twgl.bindFramebufferInfo(this.ctx, null);
+  }
+
+  #getAmbientColor(/**@type{Material}*/material) {
+    if (!material) {
+      return null;
+    }
+    if (this.lighting.diffuseAsAmbient) {
+      const dRatio = this.lighting.diffuseAsAmbient;
+      const aRatio = 1 - dRatio;
+      return twgl.v3.add(twgl.v3.mulScalar(material.Ka, aRatio), twgl.v3.mulScalar(material.Kd, dRatio));
+    }
+    return material.Ka;
   }
 }
 
--- twgl/scenes.js
+++ twgl/scenes.js
@@ -1,8 +1,6 @@
 import { fromObjFile } from '../webgl/obj-file.js';
 
     // const models = [
-    //   // fromObjFile('../models/desk/desk.obj', {position: [-1.4, -1.45, 2.3], scale: 0.2, rotateY: Math.PI / 2}),
-    //   // fromObjFile('../models/living_room/living_room.obj', {position: [0, -1.5, -4], }),
 
     //   // fromObjFile('../models/bmw/bmw.obj', {position: [0, -1.8, -4], scale: 0.3}),
     //   // fromObjFile('../models/InteriorTest/InteriorTest.obj', {position: [0, -1.8, 0]}),
@@ -30,21 +28,59 @@ export const Scenes = {
         fromObjFile('../models/breakfast_room/breakfast_room.obj', {position: [0, -2, -8]}),
       ],
       options: {
-        bgColor: [0.3, 0.9, 0.8],
+        bgColor: [0.18, 0.30, 0.40],
         lighting: {
           d: [-0.707, -0.707, -0.1],
           di: 0.3,
+          a: 0.3,
+          outsideShadowMapCoef: 0.85,
           s: [
-            [0.95, 1.5, -10, 30, 1, 0.8392, 0.6666],
-            [-2.2, 1.5, -10, 30, 1, 0.8392, 0.6666],
+            {
+              position: [0.95, 1.5, -10],
+              brightness: 200,
+              color: [1, 0.8392, 0.6666],
+            },
+            {
+              position: [-2.2, 1.5, -10],
+              brightness: 200,
+              color: [1, 0.8392, 0.6666],
+            },
           ],
-          a: 0.2,
         },
       },
     };
   },
 
-
+  LivingRoom() {
+    return {
+      models: [
+        fromObjFile('../models/living_room/living_room.obj', {position: [0, -1.5, -5], }),
+        fromObjFile('../models/desk/desk.obj', {position: [-1.4, -1.45, 1.3], scale: 0.2, rotateY: Math.PI / 2}),
+      ],
+      options: {
+        lighting: {
+          d: [0, -0.5, 1],
+          di: 0.8,
+          a: 0.15,
+          shadowCoef: 0.62,
+          shadowBias: 2.6,
+          diffuseAsAmbient: 0.6,
+          s: [
+            {
+              position: [0.25, 0.65, -2],
+              brightness: 140,
+              color: [1, 0.8928, 0.7777],
+            },
+            {
+              position: [2.5, 0.20, -3.3],
+              brightness: 100,
+              color: [1, 0.8928, 0.7777],
+            },
+          ],
+        }
+      }
+    }
+  }
 }
 
 /**
@@ -55,8 +91,17 @@ export const Scenes = {
  *    lighting: {
  *      d: [number, number, number],
  *      di: number,
- *      s: [number, number, number, number][],
+ *      a: number,
+ *      diffuseAsAmbient: number,
+ *      s: {
+ *        position: [number, number, number],
+ *        brightness: number,
+ *        color: [number, number, number],
+ *        upVec: [number, number, number],
+ *      }[],
  *    },
+ *    shadowCoef: number,
+ *    outsideShadowMapCoef: number,
  *    unlockHeight: boolean,
  *    unlockUp: boolean,
  * }
